80db1ef34277f6c64dc0f78995d6f6b3692c4a20
Michael Hof <michael.decentaddress@gmail.com>
2020-03-23 15:40:06 +0100

    changed Window-Object name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame,sys
 from pygame.locals import *
 pygame.init()
-SurfXObj=pygame.display.set_mode((400,300))
+DISPLAY_MAIN=pygame.display.set_mode((1400,300))
 pygame.display.set_caption('Main window')
 while True: #main game loop
     for event in pygame.event.get():
